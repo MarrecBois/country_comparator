160337079490838e369c2d9812f3961aa4ce0fe2
Garrett Woodson <garrettwoodson@Garretts-MacBook-Air.local>
2024-01-27 19:43:14 -0500

    Lots and lots of work

--- nextjs-flask/app.py
+++ nextjs-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -15,9 +15,21 @@ class Todo(db.Model):
 def __repr__(self):
     return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        return render_template('gameon.html', score = 0)
+   
     return render_template('index.html')
 
+@app.route('/gameon', methods=['POST', 'GET'])
+def gameon():
+    score += 1
+    return render_template('gameon.html', score = score)
+
+@app.route('/gameover', methods=['POST', 'GET'])
+def gameover():
+    return render_template('gameover.html')
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
