61d959505b76f1390cbf63e259e198e5685e802b
Garrett Woodson <garrettwoodson@Garretts-MacBook-Air.local>
2024-01-27 20:30:54 -0500

    Lots more work

--- nextjs-flask/app.py
+++ nextjs-flask/app.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template, redirect, request, session
-from flask_sqlalchemy import SQLAlchemy
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from datetime import datetime
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#db = SQLAlchemy(app)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app) 
+
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(200), nullable=False)
+#     completed = db.Column(db.Integer, default=0)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 def __repr__(self):
     return '<Task %r>' % self.id
@@ -18,17 +29,22 @@ def __repr__(self):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        return render_template('gameon.html', score = 0)
-   
-    return render_template('index.html')
+        return redirect('/gameon')
+    else:
+        return render_template('index.html')
 
 @app.route('/gameon', methods=['POST', 'GET'])
 def gameon():
-    score += 1
-    return render_template('gameon.html', score = score)
-
+    if request.method == 'POST':
+        session['score'] += 1
+        return render_template('gameon.html', score = session['score'])
+    else: 
+        session['score'] = 0
+        return render_template('gameon.html', score = 0)
 @app.route('/gameover', methods=['POST', 'GET'])
 def gameover():
+    if request.method == 'POST':
+        return redirect('/gameon')
     return render_template('gameover.html')
 
 if __name__ == "__main__":
